26d01c17dd18968012741ef3216d4b6353a94d78
keshavanand98 <keshavanand217998@gmail.com>
2020-09-09 12:05:59 +0530

    added delete update features to the blog

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 const postSchema = {
   title: String,
@@ -25,39 +25,49 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-  Post.find({}, function(err, posts){
+  Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
       posts: posts
-      });
+    });
   });
 });
 
-app.get("/compose", function(req, res){
+app.get("/admin", function (req, res) {
+
+  Post.find({}, function (err, posts) {
+    res.render("admin", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  });
+});
+
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
 
-  post.save(function(err){
-    if (!err){
-        res.redirect("/");
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
     }
   });
 });
 
-app.get("/posts/:postId", function(req, res){
+app.get("/posts/:postId", function (req, res) {
 
-const requestedPostId = req.params.postId;
+  const requestedPostId = req.params.postId;
 
-  Post.findOne({_id: requestedPostId}, function(err, post){
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
     res.render("post", {
       title: post.title,
       content: post.content
@@ -66,15 +76,43 @@ const requestedPostId = req.params.postId;
 
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/update", function (req, res) {
+  const requestedPostId = req.query.post_id;
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("update", { id: requestedPostId, title: post.title, content: post.content });
+  });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.post("/update", function (req, res) {
+  console.log(req.body);
+  Post.updateOne({ _id: req.body.postId }, {
+    $set: {
+      title: req.body.postTitle,
+      content: req.body.postBody
+    }
+  },
+  function (err, post) {
+    res.redirect('/');
+  });
+});
+
+app.post("/remove", function (req, res) {
+  const requestedPostId = req.body.post_id;
+
+  Post.deleteOne({ _id: requestedPostId }, function (err, post) {
+    res.redirect('/');
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
